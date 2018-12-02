50545fa02d20798431b543a1a11d37829441086e
Ilia <melekhavets.art@yandex.by>
2018-12-02 22:37:50 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const minify = require("gulp-minify");
+
+gulp.task('default', () => {
+    gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest("./src/style"))
+
+    gulp.src('./src/style/*.css')
+        .pipe(gulp.dest("./build/style"))
+    
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest("./build"))    
+
+    // gulp.src('./src/scripts/*.js')
+    //     .pipe(babel({
+    //         presets: ['@babel/env']
+    //     }))
+    //     .pipe(minify())
+    //     .pipe(gulp.dest("./build"))
+    
+})
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            }
+        ]
+    }
+}
